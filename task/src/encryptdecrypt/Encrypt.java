693a511ffadfd000b6dddbe5dabb94dd1eb185d1
Jeremy Grecu <jeremy.grecu@sap.com>
2021-09-16 12:07:00 +0100

    JBA - Encryption-Decryption - Stage 2/6: Knowledge is key

--- task/src/encryptdecrypt/Encrypt.java
+++ task/src/encryptdecrypt/Encrypt.java
@@ -1,6 +1,7 @@
 package encryptdecrypt;
 
 public class Encrypt {
+    private final String str = "abcdefghijklmnopqrstuvwxyz";
 
     public String encryptText(String input) {
         StringBuilder sb = new StringBuilder();
@@ -8,6 +9,20 @@ public class Encrypt {
             if (input.charAt(i) >= 'a' && input.charAt(i) <= 'z') {
                 char letter = (char) (122 - input.charAt(i) + 97);
                 sb.append(letter);
+            } else {
+                sb.append(input.charAt(i));
+            }
+        }
+        return sb.toString();
+    }
+
+    public String encryptText(String input, int key) {
+        StringBuilder sb = new StringBuilder();
+        for (int i = 0; i < input.length(); i++) {
+            if (input.charAt(i) >= 'a' && input.charAt(i) <= 'z') {
+                char letter = input.charAt(i);
+                int indexLetter = (str.indexOf(letter) + key) % 26;
+                sb.append(str.charAt(indexLetter));
             } else {
                 sb.append(input.charAt(i));
             }
--- task/src/encryptdecrypt/Main.java
+++ task/src/encryptdecrypt/Main.java
@@ -1,10 +1,17 @@
 package encryptdecrypt;
 
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        String text = "we found a treasure!";
+        Scanner sc = new Scanner(System.in);
         Encrypt encrypt = new Encrypt();
-        String encryptedText = encrypt.encryptText(text);
+        //String text = "we found a treasure!";
+        //String encryptedText = encrypt.encryptText(text);
+        String text = sc.nextLine();
+        int key = sc.nextInt();
+        String encryptedText = encrypt.encryptText(text, key);
         System.out.println(encryptedText);
+        sc.close();
     }
 }
