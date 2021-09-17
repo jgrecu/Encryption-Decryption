06eec96319ece8642a294eaf230efe550dff727f
Jeremy Grecu <jeremy.grecu@sap.com>
2021-09-17 10:56:41 +0100

    JBA - Encryption-Decryption - Stage 4/6: I command you

--- task/src/encryptdecrypt/Main.java
+++ task/src/encryptdecrypt/Main.java
@@ -1,18 +1,22 @@
 package encryptdecrypt;
 
-import java.util.Scanner;
+import java.util.HashMap;
 
 public class Main {
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        Encrypt encrypt = new Encrypt();
 
-        String process = sc.nextLine();
-        String inputText = sc.nextLine();
-        int key = sc.nextInt(); //sc.nextLine();
+        Encrypt encrypt = new Encrypt();
+        HashMap<String, String> argsMap = new HashMap<>();
+
+        for (int i = 0; i < args.length; i += 2) {
+            argsMap.put(args[i], args[i + 1]);
+        }
+
+        String process = argsMap.getOrDefault("-mode", "enc");
+        int key = Integer.parseInt(argsMap.getOrDefault("-key", "0"));
+        String inputText = argsMap.getOrDefault("-data", "");
 
         String text = encrypt.processText(inputText, key, process);
         System.out.println(text);
-        sc.close();
     }
 }
