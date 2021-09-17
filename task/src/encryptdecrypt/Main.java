77999badf9a055e17c0f4238a6063747a0834a2e
Jeremy Grecu <jeremy.grecu@sap.com>
2021-09-17 12:22:43 +0100

    JBA - Encryption-Decryption - Stage 5/6: X-files

--- task/src/encryptdecrypt/Main.java
+++ task/src/encryptdecrypt/Main.java
@@ -1,22 +1,86 @@
 package encryptdecrypt;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.PrintStream;
 import java.util.HashMap;
+import java.util.Scanner;
 
 public class Main {
+    static final EncryptDecrypt encryptDecrypt = new EncryptDecrypt();
+    static final String error = "Error";
+
     public static void main(String[] args) {
 
-        EncryptDecrypt encryptDecrypt = new EncryptDecrypt();
-        HashMap<String, String> argsMap = new HashMap<>();
+        final HashMap<String, String> argsMap = new HashMap<>();
 
-        for (int i = 0; i < args.length; i += 2) {
-            argsMap.put(args[i], args[i + 1]);
+        try {
+            for (int i = 0; i < args.length; i += 2) {
+                argsMap.put(args[i], args[i + 1]);
+            }
+        } catch (ArrayIndexOutOfBoundsException e) {
+            System.out.println(error);
+            System.exit(1);
         }
 
         String process = argsMap.getOrDefault("-mode", "enc");
         int key = Integer.parseInt(argsMap.getOrDefault("-key", "0"));
         String inputText = argsMap.getOrDefault("-data", "");
 
+        if (argsMap.get("-data")  == null && argsMap.get("-in") != null) {
+            String inFileName = argsMap.get("-in");
+            inputText = readFile(inFileName);
+        }
+
+        if (inputText == null) {
+            System.exit(1);
+        }
+
+        if (argsMap.get("-out") == null) {
+            processDataPrint(inputText, key, process);
+        } else {
+            String outFileName = argsMap.get("-out");
+            processDataToFile(inputText, key, process, outFileName);
+        }
+
+    }
+
+
+    public static void processDataPrint(String inputText, int key, String process) {
         String text = encryptDecrypt.processText(inputText, key, process);
         System.out.println(text);
+    }
+
+    public static String readFile(String filename) {
+        if (filename.equals("")) {
+            System.out.println(error);
+            System.exit(1);
+        }
+        File file = new File(filename);
+        StringBuilder fileContents = new StringBuilder((int)file.length());
+        try (Scanner scanner = new Scanner(file)) {
+            while(scanner.hasNextLine()) {
+                fileContents.append(scanner.nextLine()).append(System.lineSeparator());
+            }
+            return fileContents.toString();
+        } catch (FileNotFoundException e) {
+            System.out.println(error);
+            return null;
+        }
+    }
+
+    private static void processDataToFile(String inputText, int key, String process, String outFileName) {
+        if (outFileName.equals("")) {
+            System.out.println(error);
+            System.exit(1);
+        }
+        String text = encryptDecrypt.processText(inputText, key, process);
+        try (PrintStream out = new PrintStream(new FileOutputStream(outFileName))) {
+            out.print(text);
+        } catch (FileNotFoundException e) {
+            System.out.println(error);
+            System.exit(1);
+        }
     }
 }
